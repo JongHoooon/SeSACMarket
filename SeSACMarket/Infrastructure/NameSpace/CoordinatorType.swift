1fcef4e0c307ce43d5f14f9fd89808b8b3f5e41d
JongHoon <qnm541@gmail.com>
2023-09-07 19:16:45 +0900

    [feature] CoordinatorProtocol child 관리 메소드 추가

---
+++ SeSACMarket/Infrastructure/NameSpace/CoordinatorType.swift
@@ -0,0 +1,13 @@
+//
+//  CoordinatorType.swift
+//  SeSACMarket
+//
+//  Created by JongHoon on 2023/09/07.
+//
+
+enum CoordinatorType {
+    case app
+    case auth
+    case tabBar
+    case search
+}
--- SeSACMarket/Presentation/Base/CoordinatorProtocol.swift
+++ SeSACMarket/Presentation/Base/CoordinatorProtocol.swift
@@ -7,9 +7,23 @@
 
 import UIKit
 
-protocol CoordinatorProtocol {
+protocol CoordinatorProtocol: AnyObject {
     var childCoordinators: [CoordinatorProtocol] { get set }
+    var navigationController: UINavigationController { get }
+    var type: CoordinatorType { get }
     func start()
+}
+
+// child 관리
+extension CoordinatorProtocol {
+    func addChild(child: CoordinatorProtocol) {
+        childCoordinators.append(child)
+    }
+    
+    func removeChild(child: CoordinatorProtocol) {
+        childCoordinators = childCoordinators.filter { $0.type != child.type }
+        child.navigationController.popToRootViewController(animated: false)
+    }
 }
 
 // 화면 전환
