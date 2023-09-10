fbd86eca2106714b81780b466d9bb6b7e088740f
JongHoon <qnm541@gmail.com>
2023-09-10 14:18:02 +0900

    [feature] 정렬, query로 검색 구현

--- SeSACMarket/Presentation/SearchScene/View/SearchViewController.swift
+++ SeSACMarket/Presentation/SearchScene/View/SearchViewController.swift
@@ -20,6 +20,8 @@ final class SearchViewController: BaseViewController {
     // MARK: - UI
     private let searchBar = DefaultSearchBar()
     private let cancelButton = ButtonStyle1(title: "취소")
+    private let sortCollectionView = SortCollectionView()
+    private let productsCollectionView = ProductsCollectionView()
     
     // MARK: - Init
     init(viewModel: SearchViewModel) {
@@ -35,37 +37,147 @@ final class SearchViewController: BaseViewController {
     // MARK: - LifeCycle
     override func viewDidLoad() {
         super.viewDidLoad()
-        viewModel.viewDidLoad()
+    }
+    
+    override func viewWillLayoutSubviews() {
+        super.viewWillLayoutSubviews()
+        productsCollectionView.configureLayout()
+        sortCollectionView.configureLayout()
     }
     
     // MARK: - Configure
     override func configure() {
         super.configure()
-        
+        bind()
+        sortCollectionView.selectItem(
+            at: IndexPath(item: 0, section: 0),
+            animated: false,
+            scrollPosition: .top
+        )
         [
-            searchBar,
-            cancelButton
+            searchBar, cancelButton,
+            sortCollectionView,
+            productsCollectionView
         ].forEach { view.addSubview($0) }
     }
     
     override func configureLayout() {
         searchBar.snp.makeConstraints {
             $0.top.equalTo(view.safeAreaLayoutGuide)
-            $0.leading.equalToSuperview().inset(8.0)
+            $0.leading.equalToSuperview().inset(Constant.Inset.medium)
             $0.trailing.equalTo(cancelButton.snp.leading)
         }
         
         cancelButton.snp.makeConstraints {
             $0.centerY.equalTo(searchBar)
-            $0.trailing.equalToSuperview().inset(8.0)
+            $0.trailing.equalToSuperview().inset(Constant.Inset.medium)
         }
         cancelButton.setContentCompressionResistancePriority(
             .defaultHigh,
             for: .horizontal
         )
+        
+        sortCollectionView.snp.makeConstraints {
+            $0.top.equalTo(searchBar.snp.bottom).offset(Constant.Inset.defaultInset)
+            $0.height.equalTo(Constant.SortCell.height)
+            $0.horizontalEdges.equalToSuperview()
+        }
+        
+        productsCollectionView.snp.makeConstraints {
+            $0.top.equalTo(sortCollectionView.snp.bottom).offset(Constant.Inset.defaultInset)
+            $0.horizontalEdges.equalToSuperview()
+            $0.bottom.equalTo(view.safeAreaLayoutGuide)
+        }
     }
     
     override func configureNavigationBar() {
         navigationItem.title = "쇼핑 검색"
+    }
+}
+
+// MARK: - Bind
+private extension SearchViewController {
+    func bind() {
+        bindInput()
+        bindOutput()
+        
+        productsCollectionView.rx.didEndDisplayingCell
+            .subscribe(onNext: { event in
+                guard let cell = event.cell as? ProductCollectionViewCell else { return }
+                cell.cancelImageFetch()
+            })
+            .disposed(by: disposeBag)
+    }
+    
+    // MARK: - Input
+    func bindInput() {
+        
+        Observable.just(Void())
+            .bind(
+                with: self,
+                onNext: { owner, _ in
+                    owner.viewModel.viewDidLoad()
+            })
+            .disposed(by: disposeBag)
+        
+        sortCollectionView.rx.modelSelected(SortEnum.self)
+            .bind(
+                with: self,
+                onNext: { owner, sort in
+                    owner.viewModel.selectedSortRelay.accept(sort)
+            })
+            .disposed(by: disposeBag)
+        
+        let searchBarText = searchBar.rx.text
+            .orEmpty
+            .distinctUntilChanged()
+        
+        searchBar.rx.searchButtonClicked
+            .withLatestFrom(searchBarText)
+            .bind(
+                with: self,
+                onNext: { owner, text in
+                    owner.viewModel.searchBarTextRelay.accept(text)
+                    owner.viewModel.searchButtonClicked()
+            })
+            .disposed(by: disposeBag)
+        
+        productsCollectionView.rx.prefetchItems
+            .bind(
+                with: self,
+                onNext: { owner, indexPaths in
+                    owner.viewModel.prefetchItems(indexPaths: indexPaths)
+            })
+            .disposed(by: disposeBag)
+    }
+    
+    // MARK: - Output
+    func bindOutput() {
+        
+        viewModel.sortItemsRelay
+            .asDriver()
+            .distinctUntilChanged()
+            .drive(sortCollectionView.rx.items(
+                cellIdentifier: SortCollectionViewCell.identifier,
+                cellType: SortCollectionViewCell.self
+            )) { index, sort, cell in
+                cell.configureCell(sort: sort)
+            }
+            .disposed(by: disposeBag)
+        
+        viewModel.productsItemsRelay
+            .asDriver()
+            .drive(productsCollectionView.rx.items(
+                cellIdentifier: ProductCollectionViewCell.identifier,
+                cellType: ProductCollectionViewCell.self
+            )) { index, product, cell in
+                cell.configureCell(product: product)
+            }
+            .disposed(by: disposeBag)
+        
+        viewModel.scrollContentOffsetRelay
+            .asSignal()
+            .emit(to: productsCollectionView.rx.contentOffset)
+            .disposed(by: disposeBag)
     }
 }
--- SeSACMarket/Presentation/SearchScene/ViewModel/SearchViewModel.swift
+++ SeSACMarket/Presentation/SearchScene/ViewModel/SearchViewModel.swift
@@ -7,16 +7,26 @@
 
 import Foundation
 
+import Kingfisher
+import RxSwift
+import RxCocoa
+
 struct SearchViewModelActions {
     
 }
 
 protocol SearchViewModelInput {
     func viewDidLoad()
+    func searchButtonClicked()
+    func prefetchItems(indexPaths: [IndexPath])
+    var searchBarTextRelay: BehaviorRelay<String> { get }
+    var selectedSortRelay: BehaviorRelay<SortEnum> { get }
 }
 
 protocol SearchViewModelOutput {
-    
+    var sortItemsRelay: BehaviorRelay<[SortEnum]> { get }
+    var productsItemsRelay: BehaviorRelay<[Product]>  { get }
+    var scrollContentOffsetRelay: PublishRelay<CGPoint> { get }
 }
 
 protocol SearchViewModel: SearchViewModelInput, SearchViewModelOutput {}
@@ -25,6 +35,10 @@ final class DefaultSearchViewModel: SearchViewModel {
 
     private let productRemoteRepositoryUseCase: ProductRemoteUseCase
     private let actions: SearchViewModelActions
+    private let disposeBag = DisposeBag()
+    
+    private var page = 1
+    private var isEndPage = false
     
     init(
         productRemoteRepositoryUseCase: ProductRemoteUseCase,
@@ -36,18 +50,64 @@ final class DefaultSearchViewModel: SearchViewModel {
     
     // MARK: - Input
     func viewDidLoad() {
+        
+    }
+    
+    func searchButtonClicked() {
+        page = 1
+        isEndPage = false
+        productsItemsRelay.accept([])
+        fetchProducts()
+        scrollContentOffsetRelay.accept(.zero)
+    }
+    
+    func prefetchItems(indexPaths: [IndexPath]) {
+        let items = indexPaths.map { $0.item }
+        
+        var urls: [URL] = []
+        items.forEach {
+            if let url = URL(string: productsItemsRelay.value[$0].imageURL) {
+                urls.append(url)
+            }
+        }
+        ImagePrefetcher(resources: urls).start()
+        
+        if items.contains(productsItemsRelay.value.count - 1)
+            && isEndPage == false {
+            page += 1
+            fetchProducts()
+        }
+    }
+    
+    var searchBarTextRelay: BehaviorRelay<String> = .init(value: "")
+    var selectedSortRelay: BehaviorRelay<SortEnum> = .init(value: .similarity)
+    
+    // MARK: - Output
+    var productsItemsRelay: BehaviorRelay<[Product]> = .init(value: [])
+    var sortItemsRelay: BehaviorRelay<[SortEnum]> = .init(value: SortEnum.allCases)
+    var scrollContentOffsetRelay: PublishRelay<CGPoint> = .init()
+}
+
+private extension DefaultSearchViewModel {
+    func fetchProducts() {
         Task {
             do {
                 let productsPage = try await productRemoteRepositoryUseCase
                     .fetchProducts(
-                        query: "수박",
-                        sort: .similarity,
-                        start: 1
+                        query: searchBarTextRelay.value,
+                        sort: selectedSortRelay.value,
+                        start: page
                     )
-                print(productsPage)
+                let newItems = productsItemsRelay.value + productsPage.items
+                productsItemsRelay.accept(newItems)
+                if productsPage.items.isEmpty {
+                    isEndPage = true
+                }
             } catch {
                 print(error)
             }
         }
     }
 }
+
+
