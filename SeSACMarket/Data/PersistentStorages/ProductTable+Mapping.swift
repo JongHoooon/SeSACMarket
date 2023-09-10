f32debb36d95a7270de753cb3563cb6deddf783e
JongHoon <qnm541@gmail.com>
2023-09-10 20:16:29 +0900

    [feature] default product local repository 구현

---
+++ SeSACMarket/Data/PersistentStorages/ProductTable+Mapping.swift
@@ -0,0 +1,44 @@
+//
+//  ProductTable+Mapping.swift
+//  SeSACMarket
+//
+//  Created by JongHoon on 2023/09/10.
+//
+
+import RealmSwift
+
+final class ProductTable: Object {
+       @Persisted(primaryKey: true) var productID: Int
+       @Persisted var title: String
+       @Persisted var imageURL: String
+       @Persisted var mallName: String
+       @Persisted var price: Int
+       @Persisted var isLike: Bool
+
+    convenience init(
+        productID: Int,
+        title: String,
+        imageURL: String,
+        mallNmae: String,
+        price: Int,
+        isLike: Bool
+    ) {
+        self.init()
+        self.productID = productID
+        self.title = title
+        self.imageURL = imageURL
+        self.mallName = mallName
+        self.price = price
+        self.isLike = isLike
+    }
+    
+    func toDomain() -> Product {
+        return Product(
+            productID: productID,
+            title: title,
+            imageURL: imageURL,
+            mallName: mallName,
+            price: price
+        )
+    }
+}
---
+++ SeSACMarket/Data/Repositories/DefaultProductLocalRepository.swift
@@ -0,0 +1,112 @@
+//
+//  DefaultProductLocalRepository.swift
+//  SeSACMarket
+//
+//  Created by JongHoon on 2023/09/10.
+//
+
+import Foundation
+
+import RealmSwift
+
+final class DefaultProductLocalRepository: ProductLocalRepository {
+
+    private var realm: Realm = try! Realm()
+    private let realmTaskQueue: DispatchQueue
+
+    init?() {
+        realmTaskQueue = DispatchQueue(label: "realm-serial-queue")
+        do {
+            try realmTaskQueue.sync {
+                realm = try Realm(queue: realmTaskQueue)
+            }
+            if let url = realm.configuration.fileURL {
+                print("📁📁📁 \(String(describing: url)) 📁📁📁")
+            }
+        } catch {
+            print(error)
+            return nil
+        }
+    }
+
+    func saveLikeProduct(product: Product) async throws {
+        try await withCheckedThrowingContinuation { [weak self] continuation in
+            guard let self else { return }
+            realmTaskQueue.async {
+                let productTable = ProductTable(
+                    productID: product.productID,
+                    title: product.title,
+                    imageURL: product.imageURL,
+                    mallNmae: product.mallName,
+                    price: product.price,
+                    isLike: true
+                )
+                do {
+                    try self.realm.write {
+                        self.realm.add(productTable)
+                    }
+                    continuation.resume()
+                } catch {
+                    continuation.resume(throwing: error)
+                }
+            }
+        }
+    }
+
+    func deleteLikeProduct(productID: Int) async throws {
+        try await withCheckedThrowingContinuation { [weak self] continuation in
+            guard let self else { return }
+            realmTaskQueue.async {
+                guard let object = self.realm.object(
+                    ofType: ProductTable.self,
+                    forPrimaryKey: productID
+                ) else { return }
+                do {
+                    try self.realm.write {
+                        self.realm.delete(object)
+                        continuation.resume()
+                    }
+                } catch {
+                    continuation.resume(throwing: error)
+                }
+            }
+        }
+    }
+
+    func fetchAllLikeProducts() async -> [Product] {
+        await withCheckedContinuation { [weak self] continuation in
+            guard let self else { return }
+            realmTaskQueue.async {
+                let objects = self.realm.objects(ProductTable.self)
+                continuation.resume(returning: objects.map { $0.toDomain() })
+            }
+        }
+    }
+
+    func fetchQueryLikeProducts(query: String) async -> [Product] {
+        await withCheckedContinuation { [weak self] continuation in
+            guard let self else { return }
+            realmTaskQueue.async {
+                let objets = self.realm.objects(ProductTable.self)
+                    .where { $0.title.contains(query) }
+                continuation.resume(returning: objets.map { $0.toDomain() })
+            }
+        }
+    }
+
+    func isLikeProduct(productID: Int) async -> Bool {
+        await withCheckedContinuation { [weak self] continuation in
+            guard let self else { return }
+            realmTaskQueue.async {
+                if let _ = self.realm.object(
+                    ofType: ProductTable.self,
+                    forPrimaryKey: productID
+                ) {
+                    continuation.resume(returning: true)
+                } else {
+                    continuation.resume(returning: false)
+                }
+            }
+        }
+    }
+}
